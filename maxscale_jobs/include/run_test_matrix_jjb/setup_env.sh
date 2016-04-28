2b3316e86b01bf8f390167d44f9e70bbebeb8bae
Timofey Turenko <timofey.turenko@mariadb.com>
2016-04-28 20:28:45 +0200

    fix matrix jobs, add create_env_open, add report_sql

--- maxscale_jobs/include/run_test_matrix_jjb/setup_env.sh
+++ maxscale_jobs/include/run_test_matrix_jjb/setup_env.sh
@@ -1,6 +1,6 @@
 echo product=`echo $backend | cut -d'-' -f1 ` >> $WORKSPACE/initial_env_vars_$BUILD_ID
 echo version=`echo $backend | cut -d'-' -f2 ` >> $WORKSPACE/initial_env_vars_$BUILD_ID
-echo name=$backend-$box-$target >> $WORKSPACE/initial_env_vars_$BUILD_ID
+echo name=$name1-$backend-$box-$target >> $WORKSPACE/initial_env_vars_$BUILD_ID
 echo ci_release="ci" >> $WORKSPACE/initial_env_vars_$BUILD_ID
 echo big="no" >> $WORKSPACE/initial_env_vars_$BUILD_ID
 echo maxscale_restart="yes" >> $WORKSPACE/initial_env_vars_$BUILD_ID
