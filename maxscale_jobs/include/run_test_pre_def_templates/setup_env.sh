dd5aa6463af923166d9db0b6a498fd0b1daa9cc2
Timofey Turenko <timofey.turenko@mariadb.com>
2016-06-15 14:41:36 +0200

    a number of small fixes

--- maxscale_jobs/include/run_test_pre_def_templates/setup_env.sh
+++ maxscale_jobs/include/run_test_pre_def_templates/setup_env.sh
@@ -1,6 +1,6 @@
 echo box="predefined_template" >> $WORKSPACE/initial_env_vars_$BUILD_ID
 echo version=$template_name >> $WORKSPACE/initial_env_vars_$BUILD_ID
-echo name=$template_name-$target >> $WORKSPACE/initial_env_vars_$BUILD_ID
+echo name=$name1-$template_name >> $WORKSPACE/initial_env_vars_$BUILD_ID
 echo ci_release="ci" >> $WORKSPACE/initial_env_vars_$BUILD_ID
 echo big="no" >> $WORKSPACE/initial_env_vars_$BUILD_ID
 echo maxscale_restart="yes" >> $WORKSPACE/initial_env_vars_$BUILD_ID
