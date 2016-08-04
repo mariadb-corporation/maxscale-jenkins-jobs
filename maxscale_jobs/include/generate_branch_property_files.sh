d0cda405f385806bee927b7faba1a4b81ae00d18
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2016-08-04 18:10:09 +0300

    #7334: maxscale_jobs/include/workspace-cleanup-total.yaml generates files but build step run_test does not see them

--- maxscale_jobs/include/generate_branch_property_files.sh
+++ maxscale_jobs/include/generate_branch_property_files.sh
@@ -1,8 +1,8 @@
-branches_file="$home/run_test_branches.list"
+branches_file="$HOME/run_test_branches.list"
 if [ ! -f ${branches_file} ]; then
 	echo "Branch file ${branches_file} not found! Exiting."
 	exit 1
 fi
 property_files_dir="$WORKSPACE/branches"
 mkdir ${property_files_dir}
-cat ${branches_file} | xargs -I@ sh -c "echo 'target=@' > ${property_files_dir}'@'"
+cat ${branches_file} | xargs -I@ sh -c "echo 'target=@' > ${property_files_dir}/'@'"
