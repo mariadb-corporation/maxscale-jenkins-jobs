4fa327caf3b3d1dbaf585f2dcce49f4e5535d0bc
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2016-08-04 15:58:33 +0300

    7334: adds parameter factory

--- maxscale_jobs/include/generate_branch_property_files.sh
+++ maxscale_jobs/include/generate_branch_property_files.sh
@@ -3,7 +3,6 @@ if [ ! -f ${branches_file} ]; then
 	echo "Branch file ${branches_file} not found! Exiting."
 	exit 1
 fi
-
 property_files_dir="$WORKSPACE/branches"
 mkdir ${property_files_dir}
 cat ${branches_file} | xargs -I@ sh -c "echo 'target=@' > ${property_files_dir}'@'"
