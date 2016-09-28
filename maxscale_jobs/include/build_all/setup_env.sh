7a47fb96484dfbb9d19b677007da641ad2fec1bc
galiaf95 <galiaf1995@gmail.com>
2016-09-28 12:41:51 +0300

    #7382 fixes

--- maxscale_jobs/include/build_all/setup_env.sh
+++
@@ -1,6 +0,0 @@
-echo product=`echo $backend | cut -d'-' -f1 ` >> $WORKSPACE/initial_env_vars_$BUILD_ID
-echo version=`echo $backend | cut -d'-' -f2 ` >> $WORKSPACE/initial_env_vars_$BUILD_ID
-echo name=$name1-$backend-$box >> $WORKSPACE/initial_env_vars_$BUILD_ID
-echo ci_release="ci" >> $WORKSPACE/initial_env_vars_$BUILD_ID
-echo big="no" >> $WORKSPACE/initial_env_vars_$BUILD_ID
-echo maxscale_restart="yes" >> $WORKSPACE/initial_env_vars_$BUILD_ID
