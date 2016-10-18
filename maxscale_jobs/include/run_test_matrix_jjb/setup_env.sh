997b10e37216f3d11cea82c32d2181c1392b3de5
Timofey Turenko <timofey.turenko@mariadb.com>
2016-10-18 14:37:36 +0200

    move smoke and big parameters to separate include files

--- maxscale_jobs/include/run_test_matrix_jjb/setup_env.sh
+++ maxscale_jobs/include/run_test_matrix_jjb/setup_env.sh
@@ -2,4 +2,3 @@ echo product=`echo $backend | cut -d'-' -f1 ` >> $WORKSPACE/initial_env_vars_$BU
 echo version=`echo $backend | cut -d'-' -f2 ` >> $WORKSPACE/initial_env_vars_$BUILD_ID
 echo name=$name1-$backend-$box >> $WORKSPACE/initial_env_vars_$BUILD_ID
 echo big="no" >> $WORKSPACE/initial_env_vars_$BUILD_ID
-echo maxscale_restart="yes" >> $WORKSPACE/initial_env_vars_$BUILD_ID
--- maxscale_jobs/include/run_test_pre_def_templates/setup_env.sh
+++ maxscale_jobs/include/run_test_pre_def_templates/setup_env.sh
@@ -2,4 +2,3 @@ echo box="predefined_template" >> $WORKSPACE/initial_env_vars_$BUILD_ID
 echo version=$template_name >> $WORKSPACE/initial_env_vars_$BUILD_ID
 echo name=$name1-$template_name >> $WORKSPACE/initial_env_vars_$BUILD_ID
 echo big="no" >> $WORKSPACE/initial_env_vars_$BUILD_ID
-echo maxscale_restart="yes" >> $WORKSPACE/initial_env_vars_$BUILD_ID
