b82f0946caedcbd6a15090c612ad6bb09b260d4a
Timofey Turenko <timofey.turenko@mariadb.com>
2017-02-07 10:21:32 +0200

    Throttle (#131)
    * add upgrade test to build_all and daily_build_all
    
    * add parameters files
    
    * add descriptions
    
    * add trottle options to build and build_all
    
    * add throttle

--- create_build_and_test_alone.sh
+++ create_build_and_test_alone.sh
@@ -1,3 +1,4 @@
 cp maxscale_jobs/build_and_test.yaml maxscale_jobs/build_and_test_alone.yaml
-sed -i "s/concurrent: true/concurrent: false/" maxscale_jobs/build_and_test_alone.yaml
+#sed -i "s/concurrent: true/concurrent: false/" maxscale_jobs/build_and_test_alone.yaml
+sed -i "s/max-per-node: 5/max-per-node: 1/" maxscale_jobs/build_and_test_alone.yaml
 sed -i "s/name: build_and_test/name: build_and_test_alone/" maxscale_jobs/build_and_test_alone.yaml
