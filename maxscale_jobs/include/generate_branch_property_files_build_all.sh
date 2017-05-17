caa73902bd3356d44c32e740b26fc8f48aebb8ce
Alexander Kaluzhniy <GALIAF1995@GMAIL.COM>
2017-05-17 15:51:16 +0300

    #8175 removed source parameter and added source value streight to git… (#154)
    * #8175 removed source parameter and added source value streight to git scm section as branch parameter
    
    * #8175 renamed value to source
    
    * #8175 refactored source for git scm jenkins plugin
    
    * #8175 source variable name had conflicts if it's used as commit ID, it's replaced with scm_source
    
    * Update source.yaml
    
    * #8175 removed source/value occurances that left
    
    * 8175 added scm_source crearance
    
    * #8175 added brackets to variable scm_source_trim

--- maxscale_jobs/include/generate_branch_property_files_build_all.sh
+++ maxscale_jobs/include/generate_branch_property_files_build_all.sh
@@ -12,7 +12,7 @@ do
 	git ls-remote --heads ${repo} | grep ${branch_template} | sed 's?.*refs/heads/??' | while read branch;
 	do
 	   	if [ ${branch} == ${branch_template} ] ; then
-			echo "value=${branch}"	> ${property_files_dir}/${branch}
+			echo "scm_source=${branch}"	> ${property_files_dir}/${branch}
 		        echo "target=${branch}" >> ${property_files_dir}/${branch}
 #			echo "test_set=${test_set}" >>${property_files_dir}/${branch}
 #			echo "name=daily_maxscale_bsl_branch_run_test-$BUILD_ID-${branch}" >> ${property_files_dir}/${branch} 
